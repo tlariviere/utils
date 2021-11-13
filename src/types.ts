862c103faa36dd46fa6d9122e527d787a4f1a3a4
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2021-11-13 16:22:50 +0100

    Add express request & async handlers types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,32 @@
+import type express from "express";
+
 // ===========================================================================
 // Optional
 // ===========================================================================
 
 export type Optional<T> = T | null;
+
+// ===========================================================================
+// Request
+// ===========================================================================
+
+export interface ReqBody {
+  [key: string]: string | undefined;
+}
+
+export interface ReqCookies {
+  [key: string]: string | undefined;
+}
+
+export interface Request extends express.Request {
+  cookies: ReqCookies;
+  body: ReqBody;
+}
+
+export interface AsyncRequestHandler {
+  (
+    req: Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): Promise<void>;
+}
